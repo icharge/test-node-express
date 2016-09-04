776e3ea3953d1d2c5cbc5d6238a985fe29a2413f
Norrapat Nimmanee <charge_n@hotmail.com>
2016-09-04 22:30:02 +0700

    Initial Commit

---
+++ routes/mt4.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.get('/', function (req, res, next) {
+  res.render('mt4');
+});
+
+router.get('/load/:fileName', function (req, res, next) {
+
+  // Async respond
+  fs.readFile("files/" + req.params.fileName, "utf8", function (err, data) {
+    if (err) {
+      var err = new Error(err);
+      err.status = 500;
+      next(err);
+      return;
+    }
+
+    // Set HTTP header
+    // No caching
+    res.set({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': 0,
+      'Content-type': 'text/plain'
+    })
+
+    var resultArray = data;
+    res.send(resultArray);
+  });
+
+});
+
+module.exports = router;
